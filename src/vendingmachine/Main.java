a1a01d1850cd82c165fb1495e768be235e931d54
warwick <warwick.nortier@gmail.com>
2020-05-19 10:45:10 +0200

    added vendingmachine constructor

--- src/vendingmachine/Main.java
+++ src/vendingmachine/Main.java
@@ -2,6 +2,6 @@ package vendingmachine;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/vendingmachine/OverloadedVendingMachine.java
@@ -0,0 +1,16 @@
+package vendingmachine;
+
+public class OverloadedVendingMachine {
+
+    private int softDrinkQty;
+    private int saltySnackQty;
+    private int chocolatesQty;
+
+
+    public OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty){
+        this.softDrinkQty = softDrinkQty;
+        this.saltySnackQty = saltySnacksQty;
+        this.chocolatesQty = chocolatesQty;
+    }
+
+}
